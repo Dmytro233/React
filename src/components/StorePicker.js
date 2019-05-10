fdffdc715444c89f4dd70df7cad8667e74329503
Dmytro233 <dim4uk233@gmail.com>
2019-05-10 21:07:40 +0300

    Catch of the day
    Handling Events(change)

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -8,9 +8,9 @@ class StorePicker extends React.Component {
     // 1. Stop the form from submiting
     event.preventDefault();
     // 2. Get the text from that input
-    console.log(this.myInput.value);
+    const storeName = this.myInput.value;
     // 3. Change the page to /store/whatever-they-entered
-    this.props.history.push(`/store/something`);
+    this.props.history.push(`/store/${storeName}`);
   };
   render() {
     return (
